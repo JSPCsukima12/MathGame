62e167c38a6fb00185c5f6e250b712c7a583dde0
寺本匠吾 <ev220061@sogosurinoMacBook-Air-72.local>
2024-11-15 15:26:23 +0900

    Initial commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by 寺本匠吾 on 2024/11/15.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
